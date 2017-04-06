dce5ea0e1c048e226a31bae3740876d5678a01eb
David Hernandez <brodave318@gmail.com>
2017-04-05 19:19:45 -0700

    CRUD implementation for Admin:Post and HTML markup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
-  belongs_to :category
-  belongs_to :users
+  belongs_to :category, required: false
+  belongs_to :users, required: false
   has_many :comments
+
+  validates :title, presence: true
+  validates :category_id, presence: true
+  validates :body, presence: true
 end
